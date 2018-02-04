cce3ecd4dc8477e242714c43a48874e587919847
wildsj <32558920+wildsj@users.noreply.github.com>
2018-02-04 19:12:46 +0000

    Add files via upload

--- C999_Common/Cuffs/Script.js
+++ C999_Common/Cuffs/Script.js
@@ -1,6 +1,9 @@
 var C999_Common_Cuffs_CurrentStage = 0;
 var C999_Common_Cuffs_HasLooseCuffs = false;
 var C999_Common_Cuffs_HasKey = false;
+var C999_Common_Cuffs_CanEscape = false;
+var C999_Common_Cuffs_HasShim = false;
+var C999_Common_Cuffs_KeyOutOfReach = false;
 
 // Chapter Common - Cuffs Load
 function C999_Common_Cuffs_Load() {
@@ -13,6 +16,9 @@ function C999_Common_Cuffs_Load() {
 	// Set the correct starting stage
 	if (PlayerHasLockedInventory("Cuffs") == true) C999_Common_Cuffs_CurrentStage = 10;
 	else C999_Common_Cuffs_CurrentStage = 0;
+
+	// If the key would be out of the players reach then it would be unavailable.
+	if (C999_Common_Cuffs_KeyOutOfReach) C999_Common_Cuffs_HasKey = false;
 
 	// If the player has a loose set of cuffs
 	C999_Common_Cuffs_HasLooseCuffs = PlayerHasInventory("Cuffs");
@@ -48,6 +54,14 @@ function C999_Common_Cuffs_SelfCuff() {
 function C999_Common_Cuffs_Unlock() {
 	PlayerAddInventory("Cuffs", 1);
 	PlayerUnlockInventory("Cuffs");
+}
+
+// Chapter Common - Escape the cuffs using a hair clip
+function C999_Common_Cuffs_Escape() {
+	if (C999_Common_Cuffs_HasShim) {
+		PlayerAddInventory("Cuffs", 1);
+		PlayerUnlockInventory("Cuffs");
+	} else OverridenIntroText = GetText("NoShim");
 }
 
 // Chapter Common - Show the item image
