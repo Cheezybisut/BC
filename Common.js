353d63d808c21da0089c7a2adb23a709cc74e1d4
Ben987 <30805159+Ben987@users.noreply.github.com>
2018-05-05 18:26:47 -0400

    Allow Spanish in the Intro
    Allow Spanish in the Intro

--- Common.js
+++ Common.js
@@ -149,6 +149,7 @@ function ReadCSV(Array, FileName) {
 function GetWorkingLanguage() {
 	if ((CurrentLanguageTag == "FR") && ((CurrentChapter == "C000_Intro") || (CurrentChapter == "C001_BeforeClass") || (CurrentChapter == "C002_FirstClass") || (CurrentChapter == "C003_MorningDetention") || (CurrentChapter == "C004_ArtClass") || (CurrentChapter == "C005_GymClass") || (CurrentChapter == "C006_Isolation") || (CurrentChapter == "C007_LunchBreak") || (CurrentChapter == "C999_Common"))) return "FR";
 	if ((CurrentLanguageTag == "PL") && ((CurrentChapter == "C000_Intro"))) return "PL";
+	if ((CurrentLanguageTag == "ES") && ((CurrentChapter == "C000_Intro"))) return "ES";
 	if ((CurrentLanguageTag == "CN") && ((CurrentChapter == "C000_Intro") || (CurrentChapter == "C005_GymClass") || (CurrentChapter == "C999_Common"))) return "CN";
 	return "EN";
 }
